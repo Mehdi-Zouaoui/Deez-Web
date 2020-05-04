419782242e1e9d2fdb51da008eb2764feab2a0e5
Your Name <md1.zouaoui@gmail.com>
2020-05-04 22:29:22 +0200

    Added API function plus page search

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let deezerArray = [];
 const localStorage = window.localStorage;
 
+
 function getDataFromApi() {
     return $.ajax({
         url: `https://api.deezer.com/search?q=eminem&output=jsonp`,
@@ -15,9 +16,36 @@ function getDataFromApi() {
     });
 }
 
+function searchUserInput() {
+    let userInput = (document.getElementById('userInput').value).toLowerCase();
+    return $.ajax({
+        url: `https://api.deezer.com/search?q=${userInput}&output=jsonp`,
+        dataType: "jsonp",
+    }).then((result) => {
+        console.log("Résultat :", result.data);
+        result.data.forEach(song => {
+            deezerArray.push(song);
+        });
+
+        console.log('Array' , deezerArray)
+    });
+}
+function test() {
+    let userInput = (document.getElementById('userInput').value).toLowerCase();
+    console.log(userInput);
+}
+function createAudio(){
+    console.log(deezerArray[0]);
+    const audio = document.createElement('audio');
+    audio.src = deezerArray[0].preview;
+    audio.classList.add("audioStyle");
+    console.log(audio);
+   $('#audioList').innerHTML(audio)
+}
 
 function pushDataInStorage(){
-    getDataFromApi().then(() => {
+    console.log(`we're in`);
+    searchUserInput().then(() => {
         deezerArray.forEach((item , index) => {
             localStorage.setItem(index , JSON.stringify(item));
         })
@@ -34,5 +62,4 @@ function getSingleItem(index){
 function addToFavorite() {
     
 }
-pushDataInStorage();
-getSingleItem('2');
+// getSingleItem('2');
