a66eefdd4b1113542de14d8834f7f089e8ff8a26
Your Name <md1.zouaoui@gmail.com>
2020-05-05 13:21:22 +0200

    Function to create dynamic audio element done

--- script.js
+++ script.js
@@ -23,47 +23,75 @@ function searchUserInput() {
         dataType: "jsonp",
     }).then((result) => {
         console.log("Résultat :", result.data);
-        result.data.forEach(song => {
+        result.data.forEach((song, index) => {
             deezerArray.push(song);
-            createAudio(song.preview);
+            createAudio(index, song);
         });
 
-        console.log('Array' , deezerArray)
+        console.log('Array', deezerArray)
     });
 }
+
 function test() {
     let userInput = (document.getElementById('userInput').value).toLowerCase();
     console.log(userInput);
 }
-function createAudio(url){
 
+function createAudio(index, deezerItem) {
+    const audioDiv = document.createElement('div');
+    audioDiv.id = `audio_${index}`;
+    audioDiv.classList.add("card", "audio", "col-3", "mr-3");
+    const audioImg = document.createElement('img');
+    audioImg.src = deezerItem.album.cover_big;
+    audioImg.classList.add("card-img-top");
+    const audioTitle = document.createElement('h5');
+    const title = document.createTextNode(deezerItem.title);
+    audioTitle.appendChild(title);
+    audioTitle.classList.add("card-title");
+    const audioAlbum = document.createElement('p');
+    const album = document.createTextNode(deezerItem.album.title);
+    audioAlbum.appendChild(album);
+    audioAlbum.classList.add("card-text");
     const audio = document.createElement('audio');
-    audio.src = url;
+    audio.src = deezerItem.preview;
     audio.classList.add("audioStyle");
     audio.controls = true;
-    console.log(audio);
-    document.body.appendChild(audio);
-   $('#audioList').append(audio)
-
+    const favButton = document.createElement('button');
+    favButton.classList.add("btn", "btn-info");
+    favButton.innerHTML = "Ajouter aux favoris";
+    favButton.onclick = function () {
+        addToFavorite(index, deezerItem);
+    };
+    $('#audioList').append(audioDiv);
+    document.getElementById(`audio_${index}`).append(audioImg, audioTitle, audioAlbum, audio, favButton);
 }
 
-function pushDataInStorage(){
+function pushDataInStorage() {
     console.log(`we're in`);
     searchUserInput().then(() => {
-        deezerArray.forEach((item , index) => {
-            localStorage.setItem(index , JSON.stringify(item));
+        deezerArray.forEach((item, index) => {
+            localStorage.setItem(index, JSON.stringify(item));
         })
     })
 }
 
-function clearStorage () {
+function clearStorage() {
     localStorage.clear()
 }
 
-function getSingleItem(index){
+function getSingleItem(index) {
     console.log(localStorage.getItem(index));
 }
-function addToFavorite() {
-    
+
+function addToFavorite(index, item) {
+    localStorage.setItem(index, JSON.stringify(item));
 }
+function removeFromStorage(key){
+    localStorage.removeItem(key);
+}
+
+function getRandomFav() {
+    console.log(localStorage.getItem(Math.floor(Math.random() * ((localStorage.length - 1)))));
+}
+
 // getSingleItem('2');
