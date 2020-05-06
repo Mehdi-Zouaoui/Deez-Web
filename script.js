74753cc5ede1908c59bc85392ed8618e6f5b6891
Your Name <md1.zouaoui@gmail.com>
2020-05-07 01:28:02 +0200

    End journey

--- script.js
+++ script.js
@@ -17,18 +17,27 @@ function getDataFromApi() {
 }
 
 function searchUserInput() {
+    const searchOption = $("#searchOptions").val();
     let userInput = (document.getElementById('userInput').value).toLowerCase();
+    console.log(`https://api.deezer.com/search?q=${userInput}&order=${searchOption}&output=jsonp`);
+
     return $.ajax({
-        url: `https://api.deezer.com/search?q=${userInput}&output=jsonp`,
+        url: `https://api.deezer.com/search?q=${userInput}&order=${searchOption}&output=jsonp`,
         dataType: "jsonp",
     }).then((result) => {
-        console.log("Résultat :", result.data);
-        result.data.forEach((song, index) => {
-            deezerArray.push(song);
-            createAudio(index, song , 'audioList');
-        });
-
+        if (result.data.length) {
+            console.log("Résultat :", result.data);
+            result.data.forEach((song, index) => {
+                deezerArray.push(song);
+                createAudio(index, song, 'audioList');
+            });
+        } else {
+            $('body').append('<div id="test">Pas de résultat pour la recherche , veuillez réessayer</div>');
+        }
         console.log('Array', deezerArray)
+    }).catch(err => {
+        console.log(err);
+        $('body').append('<div id="test">Test</div>');
     });
 }
 
@@ -37,8 +46,7 @@ function test() {
     console.log(userInput);
 }
 
-function createAudio(index, deezerItem ,  divName) {
-
+function createAudio(index, deezerItem, divName) {
     const audioDiv = document.createElement('div');
     audioDiv.id = `audio_${index}`;
     audioDiv.classList.add("card", "audio", "col-3", "mr-3");
@@ -88,14 +96,21 @@ function getSingleItem(index) {
 function addToFavorite(index, item) {
     localStorage.setItem(index, JSON.stringify(item));
 }
-function removeFromStorage(key){
+
+function removeFromStorage(key) {
     localStorage.removeItem(key);
 }
+function getFavListe(){
+    const favList = {...localStorage};
+    Object.keys(favList).map(function(key, index){
+        createAudio(index,JSON.parse(favList[key]),'favList');
+    })
 
+}
 function getRandomFav() {
     const randomFav = localStorage.getItem(Math.floor(Math.random() * ((localStorage.length - 1))));
     console.log(JSON.parse(randomFav));
-    createAudio('0' , JSON.parse(randomFav), 'audioRandom');
+    createAudio('0', JSON.parse(randomFav), 'audioRandom');
 }
 
 // getSingleItem('2');
